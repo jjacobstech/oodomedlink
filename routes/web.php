dc62fcbd078ca1b3e28739bc570730316578f1b2
Jacobs Joshua <jacobsjoshua81@gmail.com>
2025-11-14 18:21:27 +0100

    UI fix

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     ]);
 });
 
-// Route::middleware('isClinic')->group(function () {
+Route::middleware('isClinic')->group(function () {
     Route::prefix('clinic')->middleware(['auth:clinic', 'verified'])->group(
         function () {
 
@@ -55,7 +55,7 @@ Route::get('/', function () {
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
-// });
+});
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
