8d20965f69693fdc44889a6a5a8c8e5e4dbc6972
Jacobs Joshua <jacobsjoshua81@gmail.com>
2025-11-15 23:58:48 +0100

    notification bell

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -3,6 +3,8 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/Components/Dashboard/AppSidebar.vue";
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { usePage } from "@inertiajs/vue3";
+import Badge from "@/components/ui/badge/Badge.vue";
+import { Bell } from "@solar-icons/vue";
 
 const auth = usePage().props.auth;
 
@@ -16,20 +18,32 @@ const auth = usePage().props.auth;
         <main class="bg-white h-screen w-full">
             <div class="p-5 bg-primaryDark/50 flex  justify-between items-center px-10">
                 <SidebarTrigger
-                    class=" hover:-translate-y-1  transition-all duration-150 text-white hover:bg-white hover:text-primaryDark" />
+                    class=" hover:-translate-y-1  transition-all duration-150 text-white  hover:bg-white hover:text-primaryDark" />
 
-                <Avatar>
-                    <AvatarImage src="/assets/doctor.jpeg" alt="@unovue" />
-                    <AvatarFallback
-                        class="bg-white text-transparent w-16 font-extrabold text-xl  justify-center flex items-center rounded-full h-16">
-                        <p class="bg-deepgradient bg-clip-text">
-                            {{
-                            auth.user.name.charAt(0).toUpperCase() }}
-                        </p>
+                <div class="flex items-center justify-between gap-16 ">
+                    <div class="flex relative items-center">
+                        <Bell class="w-8 h-8 text-primaryLight fill-primaryDark " />
+                        <Badge variant="outline"
+                            class="ml-4 animate-bounce bg-primaryDark border-primaryDark mb-3 absolute">
+                            <span class="text-sm font-extrabold text-white">
+                                {{ 3 }}
+                            </span>
+                        </Badge>
+                    </div>
+                    <Avatar>
+                        <AvatarImage src="/assets/doctor.jpeg" alt="@unovue" />
+                        <AvatarFallback
+                            class="bg-white text-transparent w-16 font-extrabold text-xl  justify-center flex items-center rounded-full h-16">
+                            <p class="bg-deepgradient bg-clip-text">
+                                {{
+                                auth.user.name.charAt(0).toUpperCase() }}
+                            </p>
 
-                    </AvatarFallback>
-                </Avatar>
+                        </AvatarFallback>
+                    </Avatar>
+                </div>
             </div>
+
             <slot />
             <!-- Footer -->
             <footer class="">
--- resources/js/components/ui/sidebar/utils.ts
+++ resources/js/components/ui/sidebar/utils.ts
@@ -5,7 +5,7 @@ export const SIDEBAR_COOKIE_NAME = "sidebar_state";
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 export const SIDEBAR_WIDTH = "14rem";
 export const SIDEBAR_WIDTH_MOBILE = "18rem";
-export const SIDEBAR_WIDTH_ICON = "4rem";
+export const SIDEBAR_WIDTH_ICON = "14rem";
 export const SIDEBAR_KEYBOARD_SHORTCUT = "b";
 
 export const [useSidebar, provideSidebarContext] = createContext<{
